3e78bd25ca64b36f5e5f00e46898b36cfc2c2c38
SeppeHannen <seppe.hannen1@hotmail.com>
2025-06-09 00:04:29 +0200

    Remove old files

--- utils.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import open3d as o3d
-
-
-def load_point_cloud(path: str) -> np.ndarray:
-    """Load a point cloud file (.pcd/.ply) and return Nx3 numpy array."""
-    pcd = o3d.io.read_point_cloud(str(path))
-    return np.asarray(pcd.points)
